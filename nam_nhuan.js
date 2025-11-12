b3cd61a29a9498d95aa1864359fbfa604abb1741
nguyenthanhlongvn03 <nguyenthanhlongvn03@gmail.com>
2025-11-12 15:01:46 +0700

    add

--- nam_nhuan.js
+++ nam_nhuan.js
@@ -3,20 +3,12 @@ function kiemtra(){
     let result;
     //let year = parseInt(prompt('Nhập số năm hợp lệ'));
     if(year >= 0){
-        if(year % 4 === 0 && year % 100 !== 0){
+        if(year % 4 === 0 && year % 100 !== 0 || year % 400 === 0){
             result = year +' Là năm nhuận';
         }
-        else if (year % 100 === 0){
-            if (year % 400 === 0){
-                result = year +' Là năm nhuận';
-            }
             else {
                 result = year + ' Không phải năm nhuận';
             }
-        }
-        else{
-            result = year + ' Không phải năm nhuận';
-        }
     }
     else{
         result = year + ' Là năm không hợp lệ';
